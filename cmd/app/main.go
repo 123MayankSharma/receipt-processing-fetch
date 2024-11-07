ac9d2cf30f74fdc34a15d23aeee436115f0996d5
123MayankSharma <mayanksharma922001@gmail.com>
2024-11-08 00:41:10 +0530

    Upload Receipt API Done

---
+++ cmd/app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/123MayankSharma/receipt-processor-challenge/internal/api"
+)
+
+func main() {
+	fmt.Println("------------Welcome to Receipt Processor API ------------")
+	fmt.Println("Please Refer Below for the available endpoints")
+	const PORT = 8000
+
+	fmt.Printf("* http://localhost:%d/receipts/process-------->\n", PORT)
+	fmt.Printf("* http://localhost:%d/receips/{id}/points-------->\n", PORT)
+	fmt.Println("{id} denotes the Receipt id received after uploading the receipt")
+
+	api.UploadHandler()
+
+	http.ListenAndServe(":8000", nil)
+
+}
